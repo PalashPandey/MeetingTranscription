4492e4d5374190f574701c62fed2dba144d8a7ae
Palash Pandey <pxp142@jefferson.edu>
2019-11-01 15:52:58 -0400

    added requirments

--- recieve_sound_server.py
+++ recieve_sound_server.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, render_template, request, jsonify, redirect, session, url_for
 from flask_oauthlib.client import OAuth, OAuthException
 import pandas as pd
-import what3words 
+
 import math
 import json
 
